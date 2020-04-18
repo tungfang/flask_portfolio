78e8b84d3cc3f3661f7a181c0df57afc8a5c56c7
tungfang <tungfang@tungfang.HP>
2020-04-18 11:28:35 +0800

    fix file name mismatching

--- portfolio.py
+++ portfolio.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home.html')
 def home():
 	return render_template('home.html')
 
-@app.route('/cv')
+@app.route('/cv.html')
 def cv():
 	return render_template('cv.html')
 
